8dc0db0e4dfdb5ab5caa595850484a3a12c5cf6d
letruxux <102257756+letruxux@users.noreply.github.com>
2024-05-26 16:24:05 +0200

    Update ts-setup.bat

--- ts-setup.bat
+++ ts-setup.bat
@@ -7,6 +7,7 @@ call npm i -g json
 call json -I -f package.json -e "this.main = './src/index.ts'"
 call json -I -f package.json -e "this.scripts.dev = 'npx nodemon .'"
 call json -I -f package.json -e "this.scripts.start = 'npx ts-node .'"
+call npm uninstall json
 call npm i typescript nodemon ts-node --save-dev
 call npx tsc --init
 copy nul ./src/index.ts
